e30caaf14aad2e8d915f53f59338769486d7389a
Rafael Fariñas <rafael.farinas@mi.unc.edu.ar>
2026-02-05 21:17:14 -0300

    Compilamos el programa

--- Puntero.cpp
+++ Puntero.cpp
@@ -1,13 +1,17 @@
 #include <cstdio>
 
 int main (){
-    int x = 110;
-    int * ptr = nullptr;
-    printf ("La direccion del puntero es: %p", (void*)ptr);
+    int x = 110; // integer 
+    int * ptr = nullptr; // pointer to integer
+    printf ("La direccion del puntero es: %p\n", (void*)ptr);
     ptr = &x;
-    printf ("ptr --> La dirrecion del puntero es: ",ptr); 
-    printf ("&a --> La dirrecion del puntero es: ",&x);
-    printf ( "&prt --> La direccion en que esta almacenado el puntero es: ", &ptr);
-    printf ( "*ptr -->  El valor de la direccion que almacena el puntero es: ", *ptr);
-    
+    // Para direcciones de memoria (ptr, &x, &ptr) usa %p
+    printf("ptr --> La direccion del puntero es: %p\n", ptr);
+    printf("&a  --> La direccion de x es: %p\n", &x); 
+    printf("&ptr --> La direccion donde se almacena el puntero es: %p\n", &ptr);
+
+    // Para el VALOR al que apunta (*ptr), asumiendo que es un entero (int), usa %d
+    printf("*ptr --> El valor de la direccion que almacena el puntero es: %d\n", *ptr);
+    return 0;
+
 }
