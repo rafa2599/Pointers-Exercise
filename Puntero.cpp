b38fa513bd72668407ce76f0862cafad8a9b9cd4
Rafael Fariñas <rafael.farinas@mi.unc.edu.ar>
2026-02-05 21:02:07 -0300

    Agregamos prints

--- Puntero.cpp
+++ Puntero.cpp
@@ -2,8 +2,12 @@
 
 int main (){
     int x = 110;
-    int * prt = nullptr;
-    printf ("La direccion del puntero es: %p", (void*)prt);
-    prt = &x;
-    printf ("La dirrecion del puntero es: %p", (void*)*prt); 
+    int * ptr = nullptr;
+    printf ("La direccion del puntero es: %p", (void*)ptr);
+    ptr = &x;
+    printf ("ptr --> La dirrecion del puntero es: ",ptr); 
+    printf ("&a --> La dirrecion del puntero es: ",&x);
+    printf ( "&prt --> La direccion en que esta almacenado el puntero es: ", &ptr);
+    printf ( "*ptr -->  El valor de la direccion que almacena el puntero es: ", *ptr);
+    
 }
