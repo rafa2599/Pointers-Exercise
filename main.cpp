af832fee36bca7e9f49f9493862aaa182ee20c9d
Rafael Fariñas <rafael.farinas@mi.unc.edu.ar>
2026-02-07 16:55:19 -0300

    Agregamos un nuevo archivo

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+struct Item
+{
+    string m_nombre;
+    int m_cantidad; 
+};
+
+int main (int argc , char * argv[] ) {
+    int numeroItems; 
+    cout <<"Ingrese el numero de elementos"<<endl;
+    cin >> numeroItems;
+    
+    return 0;
+
+}
