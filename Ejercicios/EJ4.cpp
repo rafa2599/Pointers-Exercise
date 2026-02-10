b5e83bce8b7ac4a9b87d465ca184bbda68e39cf3
Rafael Fariñas <rafael.farinas@mi.unc.edu.ar>
2026-02-10 00:58:41 -0300

    Agregamos algunos ejercicios para practicar

---
+++ Ejercicios/EJ4.cpp
@@ -0,0 +1,35 @@
+/*4. Memoria dinámica
+
+    Reserva memoria dinámica para un double usando new.
+
+    Asigna un valor y luego libéralo con delete.
+
+    Haz lo mismo para un arreglo de 5 enteros.
+
+    Pregunta: ¿Qué pasa si olvidas liberar la memoria?
+
+5. Arrays y punteros
+
+    Declara un arreglo int v[5] = {1,2,3,4,5};.
+
+    Usa un puntero para recorrer el arreglo e imprimir cada valor y su dirección.
+
+    Pregunta: ¿Qué diferencia hay entre v[i] y *(v+i)?
+
+6. Structs y operador flecha
+
+    Define un struct Persona con nombre y edad.
+
+    Crea una instancia y un puntero a esa instancia.
+
+    Asigna valores usando el operador ->.
+
+    Pregunta: ¿Qué ocurre si intentas acceder con . en lugar de ->?
+
+7. Función que devuelve puntero
+
+    Escribe una función que reciba un arreglo de enteros y devuelva un nuevo arreglo con cada valor multiplicado por 3 (usa new).
+
+    Imprime los valores del nuevo arreglo en main.
+
+    Pregunta: ¿Qué sucede si intentas devolver un puntero a un arreglo local (no dinámico)?*/
