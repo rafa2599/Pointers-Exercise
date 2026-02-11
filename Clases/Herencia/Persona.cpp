4139c13a6c5ee5423bdc27e01003139a143cb28f
Rafael Fariñas <rafael.farinas@mi.unc.edu.ar>
2026-02-11 11:24:05 -0300

    Organizamos el repo y agregamos el archivo Persona.cpp

---
+++ Clases/Herencia/Persona.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <cstdio>
+#include <iostream>
+#include <cstdlib>
+
+using namespace std; 
+
+class Persona {
+    private: 
+        int edad;
+        string nombre;
+
+
+    public:
+        Persona(){
+            edad = 0;
+            nombre = "Sin nombre";
+            cout << "\nConstructor por defecto "<< endl;
+        }
+
+        Persona (int edadParametro, const string& nombreParametro){
+            edad = edadParametro;
+            nombre = nombreParametro;
+            cout << "\nConstructor con parametros"<< endl;
+        }
+
+        Persona (const Persona &otra ){
+            edad = otra.edad;
+            nombre = otra.nombre;
+            cout << "\nConstructor de copia"<< endl;
+
+        }
+        Persona (Persona &&otra){
+            edad = otra.edad;
+            nombre = move(otra.nombre);
+            cout << "\nConstructor de movimiento"<< endl;
+        }
+
+
+        ~Persona (){
+            cout << "\nDestructor llamado para "<<nombre<< endl;
+        }
+        
+
+
+};
